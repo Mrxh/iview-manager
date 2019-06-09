d0e1e40965f7cf6e1bf41631cdd049a3217a932d
Mrxh <2929314899@qq.com>
2019-06-09 23:34:26 +0800

    主页面模板

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,58 @@
 <template>
-  <div style="width: 100px;height: 1000px;background:#3b8070">index</div>
+  <div id="myChart" :style="{width: '1161px', height: '300px'}"></div>
 </template>
 
 <script>
+  import echarts from 'echarts'
   export default {
+    mounted(){
+      this.drawLine();
+    },
+    methods: {
+      drawLine(){
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('myChart'))
+        // 绘制图表
+        myChart.setOption({
+          title: { text: '在Vue中使用echarts' },
+          color: ['#2D8CF0'],
+          tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+              type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis : [
+            {
+              type : 'category',
+              data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              axisTick: {
+                alignWithLabel: true
+              }
+            }
+          ],
+          yAxis : [
+            {
+              type : 'value'
+            }
+          ],
+          series : [
+            {
+              name:'直接访问',
+              type:'bar',
+              barWidth: '60%',
+              data:[10, 52, 200, 334, 390, 330, 220]
+            }
+          ]
+        });
+      }
+    }
   }
 </script>
 
